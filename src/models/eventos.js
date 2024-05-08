50f5da3d65800222a9fbdc9c6d1962298f60203a
Mariana <mariana.ospinap@konradlorenz.edu.co>
2024-05-08 18:32:10 -0500

    Creación modelo "Eventos"
    Se creo el modelo de Eventos para gestionar cada evento creado por los usuarios con su ID y demás.

--- src/models/eventos.js
+++ src/models/eventos.js
@@ -1,1 +1,24 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+const EventoSchema = mongoose.Schema({ //Creamos Schema
+    
+    titulo: {
+        type: String, 
+        required: true 
+    },
+   
+    descripcion: {
+        type: String, 
+        required: true 
+    },
+   
+    participantes: [{
+        idUsuario: {
+            type: Schema.Types.ObjectId, // Tipo ObjectID
+            ref: 'Usuario', // Referencia al modelo Usuario
+            required: true // Campo obligatorio
+        },
+    }]
+});
 
+// Exportar el modelo Evento con el esquema definido
+module.exports = mongoose.model("Evento", EventoSchema);
