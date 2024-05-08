53ccd4c925464a9971d4d174b541e685535ad2a0
Mariana <mariana.ospinap@konradlorenz.edu.co>
2024-05-08 18:34:37 -0500

    Creación modelo "Contenidos"
    Se creo el modelo Contenidos, con el fin de guardar todos los contenidos creados por los usuarios, con sus respectivos textos y demás.

--- src/models/contenidos.js
+++ src/models/contenidos.js
@@ -1,1 +1,36 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+const ContenidoSchema = mongoose.Schema({
+
+    realidadVirtual: {
+        type: [string], //URL DE LA IMAGEN DEL QR
+        required: false
+    },
+    simulacion: {
+        type: [String], //URL VIDEOS
+        required: false
+    },
+    artículos: {
+        type: String,
+        required: false
+    },
+    cursos: {
+        type: String,
+        required: true
+    },
+    tutoriales: {
+        type: String,
+        required: true
+    },
+    titulo: {
+        type: String,
+        required: true
+    },
+    descripcion: {
+        type: String,
+        required: true
+    }
+
+});
+
+module.exports = mongoose.model("Contenido", ContenidoSchema);
 
