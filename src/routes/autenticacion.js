e678a957629cd118d23591cbb8e658256f428877
Valero Vanegas - Juan David <juan.valero.9c@gmail.com>
2024-05-10 12:39:19 -0500

    Añadido Estructura de Lógin
    Para validar que un usuario tenga cifrada su sesión  en la cuenta, se implemento la estructura de su inicio de sesión.
    
    
    
    
    
    
    Pero comentareada previa a su modificación para un uso correcto

--- src/routes/autenticacion.js
+++ src/routes/autenticacion.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Usuario = require("../models/usuarios"); // Importar el modelo Usuario
+/**/const bcrypt = require("bcrypt");
 
 router.post('/signup', async (req, res) => {
     const { nombre, apellido, correo, clave, nacionalidad, edad, telefono, intereses } = req.body;
@@ -20,17 +21,43 @@ router.post('/signup', async (req, res) => {
 
         // Cifrar la contraseña antes de guardarla en la base de datos
         nuevoUsuario.clave = await nuevoUsuario.encryptClave(nuevoUsuario.clave);
+        
 
         // Guardar el nuevo usuario en la base de datos
         await nuevoUsuario.save();
 
         // Devolver el nuevo usuario en formato JSON como respuesta
         res.json(nuevoUsuario);
+
+        /*res.json({
+            message: "Usuario guardado."
+        });*/
+
     } catch (error) {
         // Manejar cualquier error y enviar una respuesta de error al cliente
         console.error("Error al registrar usuario:", error);
         res.status(500).json({ error: "Hubo un error al registrar el usuario." });
     }
 });
+
+/*router.post("/login", async (req, res) => {
+  // validaciones
+  const { error } = userSchema.validate(req.body.correo, req.body.clave);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+  //Buscando el usuario por su dirección de correo
+  const user = await userSchema.findOne({ correo: req.body.correo });
+  //validando si no se encuentra
+  if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
+  //Transformando la contraseña a su valor original para 
+  //compararla con la clave que se ingresa en el inicio de sesión
+  const validPassword = await bcrypt.compare(req.body.clave, user.clave);
+  if (!validPassword)
+    return res.status(400).json({ error: "Clave no válida" });
+  res.json({
+    error: null,
+    data: "Bienvenido(a)",
+  });
+});
+ */
 
 module.exports = router;
