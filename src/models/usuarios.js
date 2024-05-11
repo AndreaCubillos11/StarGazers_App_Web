c8ea056d9713a5cf614918465d78a71ecb6f2f8b
Ivonne <sierrarnatis@gmail.com>
2024-05-11 11:24:38 -0500

    Se agrego el campo clave en el esquema de usuario
    Para que el campo de clave funcione correctamente y se pueda encriptar se agrega un método para cifrar la contraseña antes de almacenarla en la base de datos y un método de middleware de mongoose para cifrar la contraseña antes de guardarla en la base de datos

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,53 +1,64 @@
-const mongoose = require("mongoose"); // importando el componente mogoose
-const bcrypt = require("bcrypt"); //Se importa el componente bcrypt
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const UsuarioSchema = mongoose.Schema({
- 
-        nombre: {
-            type: String,
-            required: true
-        },
-        apellido: {
-            type: String,
-            required: true
-        },
-        correo: {
-            type: String,
-            required: true
-        },
-        nacionalidad: {
-            type: String,
-            required: true
-        },
-        edad: {
-            type: Number,
-            required: true
-        },
-        telefono: {
-            type: Number,
-            required: true
-        },
-        intereses: {
-            type: String,
-            required: true
-        },
-        seguidores: {
-            type: Number,
-            required: false,
-            private: true
-        },
-        rol: {
-            type: Number,
-            required: false,
-             // Por defecto, los usuarios tienen rol de usuario
-        }
-    });
-    
-    // Método para cifrar la contraseña antes de almacenarla en la base de datos
-    UsuarioSchema.methods.encryptClave = async function(clave) {
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(clave, salt);
-        return hash;
-    };
-    
-    module.exports = mongoose.model("Usuario", UsuarioSchema);
-    
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellido: {
+        type: String,
+        required: true
+    },
+    correo: {
+        type: String,
+        required: true
+    },
+    nacionalidad: {
+        type: String,
+        required: true
+    },
+    edad: {
+        type: Number,
+        required: true
+    },
+    telefono: {
+        type: Number,
+        required: true
+    },
+    intereses: {
+        type: String,
+        required: true
+    },
+    seguidores: {
+        type: Number,
+        required: false,
+        private: true
+    },
+    rol: {
+        type: Number,
+        required: false,
+        default: 1 // Por defecto, los usuarios tienen rol de usuario
+    },
+    clave: {
+        type: String,
+        required: true 
+    }
+});
+
+// Método para cifrar la contraseña antes de almacenarla en la base de datos
+UsuarioSchema.methods.encryptClave = async function(clave) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(clave, salt);
+    return hash;
+};
+
+// Middleware de mongoose para cifrar la contraseña antes de guardarla en la base de datos
+UsuarioSchema.pre('save', async function(next) {
+    if (this.isModified('clave')) {
+        this.clave = await this.encryptClave(this.clave);
+    }
+    next();
+});
+
+module.exports = mongoose.model("Usuario", UsuarioSchema);
